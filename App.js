a22fbb6bba337e4240b99216ac9618a229e9e2ea
Aandi1989 <navaliny1989@gmail.com>
2023-12-18 16:48:00 +0300

    Detecting Orientation Changes

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+  Platform,
+  StatusBar
+} from "react-native";
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  const landscape = useDeviceOrientation() === "landscape";
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: '100%',
+        height: landscape ? '100%' : '30%',
+      }}></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
+
+// The function useDimensions() doesn’t exist anymore in the module “@react-native-community/hooks”.
+// You can use the equivalent function useWindowDimensions() from “react-native”.
+
+// import {useWindowDimensions} from "react-native";
