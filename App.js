3982da516e63d840a251b7fe6a371848b6fbd6cd
Aandi1989 <navaliny1989@gmail.com>
2023-12-26 13:42:16 +0300

    Building AuthNavigator

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from "react";
 import { Text, Button } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Screen from "./app/components/Screen";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 const Link = () => {
   const navigation = useNavigation();
@@ -38,12 +41,18 @@ const Stack = createStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen 
-      name="TweetDetails"  
-      component={TweetDetails} 
-      options={({ route }) => ({ title: route.params.id })}
-    />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
   </Stack.Navigator>
+);
+
+const Account = () => <Screen><Text>Account</Text></Screen>
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={FeedNavigator}/>
+    <Tab.Screen name="Account" component={AccountNavigator}/>
+  </Tab.Navigator>
 );
 
 export default function App() {
@@ -51,7 +60,7 @@ export default function App() {
     <NavigationContainer>
       {/* без GestureHandlerRootView не работало удаление по свайпу (появление корзинки при удалении) */}
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <StackNavigator />
+       <AuthNavigator />
       </GestureHandlerRootView>
     </NavigationContainer>
   );
@@ -63,6 +72,8 @@ export default function App() {
 // import {useWindowDimensions} from "react-native";
 
 
-// have been used in demo
+
+// have been used in demo, I installed react-navigation/stack: 5.10.0 and react-navigation/bottom-tabs:5.10.0 to fix annoying warnings
 // "@react-navigation/native": "^5.3.2",
 // "@react-navigation/stack": "^5.3.5",
+// "@react-navigation/bottom-tabs": "^5.4.4",
